bb415dcfb25a7f179c81e450153be7858bd0b83c
Naveen Gaur <naveensgour@gmail.com>
2020-06-03 19:59:38 +0530

    app.js and sample files to run app are added.

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./modules/user");
+mongoose.connect('mongodb://localhost:27017/project-buddy', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//  app configurations
+var app = express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(require("express-session")({
+    secret:"This is used encode and decode session,this can be anything",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//  passport configuration
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+//=======================
+//******  ROUTES  *******
+//=======================
+
+
+
+//  show sign up form
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+
+//  handling user sign up
+app.post("/register",function(req,res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+
+// LOGIN ROUTES
+// render login form
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+//login logic
+app.post("/login",passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}),function(req,res){
+});
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/secret",isLoggedIn,function(req,res){
+    res.render("secret");
+});
+
+app.get("/logout",function(req,res){
+    req.logOut();
+    res.redirect("/");
+});
+
+//  function is used to check to whether user is logged in or not
+//  this is used as middle function
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+app.listen(3000,()=>{
+    console.log("Server is started on port 3000");
+});
