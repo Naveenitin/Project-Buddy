9df08376f6533d999e68815ee0d1dc9a7e35d0b1
Naveen Gaur <naveensgour@gmail.com>
2020-06-05 02:23:01 +0530

    UI enhancements

--- modules/project.js
+++ modules/project.js
@@ -3,7 +3,9 @@ var mongoose = require("mongoose");
 var projectSchema = new mongoose.Schema({
     name: String,
     type: String,
+    image: String,
     idea: String,
+    username: String,
     description: String,
     url: String,
     date:{type: Date, default: Date.now},
